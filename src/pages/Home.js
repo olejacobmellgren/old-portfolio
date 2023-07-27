92ee63f30c061aa1b0a92e6a1f5c4b6ad88bfa7c
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2023-07-27 11:45:40 +0200

    setup different pages

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is the Home page content.</p>
+    </div>
+  );
+};
+
+export default Home;
