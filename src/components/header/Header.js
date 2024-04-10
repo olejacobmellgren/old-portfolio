d7402bf5456f47c8245a9145268508d74cbac00e
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2024-04-10 19:41:09 +0200

    fix styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -15,41 +15,42 @@ const Header = () => {
   }
 
   return (
-    <div>
-      <div className='marg' id='top'>
-        <input className='check' type='checkbox' id='check' />
-        <label className='checkbtn' htmlFor='check'>
-          <img className='pic' src="https://www.pngkit.com/png/full/239-2394744_icon-open-nav-icon-white.png" alt="" />
-        </label>
-        <ul className="menu">
-          <NavLink to="/" >
-            <button onClick={removeMenu} className={location.pathname === '/' ? 'selected' : ''}>
-              Om meg
-            </button>
-          </NavLink>
-          <NavLink to="/projects">
-            <button onClick={removeMenu} className={location.pathname === '/projects' ? 'selected' : ''} >
-              Prosjekter
-            </button>
-          </NavLink>
-          <NavLink to="/contact">
-            <button onClick={removeMenu} className={location.pathname === '/contact' ? 'selected' : ''}>
-              Kontakt meg
-            </button>
-          </NavLink>
-          <div className="switch">
-            <input
-              className="toggle"
-              type="checkbox"
-              checked={darkMode}
-              onClick={(e) => e.stopPropagation()}
-              onChange={(e) => {
-                setDarkMode(e.target.checked);
-              }}
-            />
-          </div>
-        </ul>
-      </div>
+    <div className='header' id='top'>
+      <input className='check' type='checkbox' id='check' />
+      <label className='checkbtn' htmlFor='check'>
+        <img className='pic' src="https://www.pngkit.com/png/full/239-2394744_icon-open-nav-icon-white.png" alt="" />
+      </label>
+      <ul className="menu">
+        <div className='menu-items'>
+        <NavLink to="/" >
+          <button onClick={removeMenu} className={location.pathname === '/' ? 'selected' : 'unselected'}>
+            Om meg
+          </button>
+        </NavLink>
+        <NavLink to="/projects">
+          <button onClick={removeMenu} className={location.pathname === '/projects' ? 'selected' : 'unselected'} >
+            Prosjekter
+          </button>
+        </NavLink>
+        <NavLink to="/contact">
+          <button onClick={removeMenu} className={location.pathname === '/contact' ? 'selected' : 'unselected'}>
+            Kontakt meg
+          </button>
+        </NavLink>
+        </div>
+        
+        <div className="switch">
+          <input
+            className="toggle"
+            type="checkbox"
+            checked={darkMode}
+            onClick={(e) => e.stopPropagation()}
+            onChange={(e) => {
+              setDarkMode(e.target.checked);
+            }}
+          />
+        </div>
+      </ul>
     </div>
   );
 };
